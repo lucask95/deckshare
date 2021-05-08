df7f6637d475187b5e4d79fb6b85e8741e6387fa
Lucas Keller <ljkeller@ucdavis.edu>
2021-05-08 10:03:29 -0700

    addCard logic correction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { Typography } from "@material-ui/core";
-import React, { useState } from "react";
-import Card from "../models/cardModel";
-import { DeckCards } from "../models/deckModel";
-import CardList from "../src/CardList";
-import DeckStats from "../src/DeckStats";
-import Layout from "../src/Layout";
-import CardSearch from "../src/Search";
+import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import Card from '../models/cardModel';
+import { DeckCards } from '../models/deckModel';
+import CardList from '../src/CardList';
+import DeckStats from '../src/DeckStats';
+import Layout from '../src/Layout';
+import CardSearch from '../src/Search';
 
 export default function Index() {
   const [deckList, setDeckList] = useState<DeckCards>({});
@@ -26,14 +26,16 @@ export default function Index() {
   };
 
   const addCard = (card: Card) => {
-    setCardAmount(card, 1);
+    if (deckList[card.card_name])
+      setCardAmount(card, deckList[card.card_name].count + 1);
+    else setCardAmount(card, 1);
   };
 
   return (
     <Layout>
       {/* search */}
       <div style={{ marginBottom: 35 }}>
-        <Typography variant='h6' gutterBottom>
+        <Typography variant="h6" gutterBottom>
           Search
         </Typography>
         <CardSearch addCard={addCard} />
@@ -41,7 +43,7 @@ export default function Index() {
 
       {/* cards */}
       <div style={{ flex: 1 }}>
-        <Typography variant='h6' gutterBottom>
+        <Typography variant="h6" gutterBottom>
           Decklist
         </Typography>
         <CardList cards={deckList} setCardAmount={setCardAmount} />
@@ -49,7 +51,7 @@ export default function Index() {
 
       {/* stats */}
       <div style={{ marginTop: 35 }}>
-        <Typography variant='h6' gutterBottom>
+        <Typography variant="h6" gutterBottom>
           Statistics
         </Typography>
         <DeckStats deckList={deckList} />
