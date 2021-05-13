b62b0c1b31ce9e3771273ea3a2f258566352de45
Lucas <ljkeller@ucdavis.edu>
2021-05-12 22:06:43 -0700

    Serve over https

--- util/config.js
+++ util/config.js
@@ -1,5 +1,5 @@
 const inProduction = process.env.NODE_ENV === "production";
 const serverUrl = inProduction
-  ? "http://localhost:3000"
-  : "http://localhost:3000";
+  ? "https://localhost:3000"
+  : "https://localhost:3000";
 export default serverUrl;
